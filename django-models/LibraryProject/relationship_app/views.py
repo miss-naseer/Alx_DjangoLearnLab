0fa656970565feeca640b29944e0bc0721261d8d
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-07-26 23:43:02 +0100

    modified django-models/LibraryProject/relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,13 @@ def list_books(request):
     books = Book.objects.select_related('author').all()  # fetch all books with their author
     return render(request, 'list_books.html', {'books': books})
 
+
+from django.views.generic import DetailView
+from .models import Library
+
+# Class-Based View to Show Library Details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'  # This is the HTML template to render
+    context_object_name = 'library'  #  This is how the library will be referenced in the template
+
