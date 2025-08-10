3a196e3fa989b35c014ae30a91cdf33998d03ec1
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-08-10 22:20:48 +0100

    updates security implementation in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
