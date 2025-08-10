5cd76b3b09fb12d5de7020147dcfb66438953025
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-08-10 22:12:41 +0100

    updated security implementation

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,3 +4,8 @@ from .models import Book
 
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
