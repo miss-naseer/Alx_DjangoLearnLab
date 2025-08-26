53430bb1c8c695151cf3f121806842bbf193cdff
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-08-26 12:27:07 +0100

    modified views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions, viewsets, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters
 from .models import Book
 from .serializers import BookSerializer
+
 
 # List all books OR create a new one
 class BookListCreateView(generics.ListCreateAPIView):
@@ -91,3 +93,5 @@ Examples:
     # Ordering fields
     ordering_fields = ['title', 'publication_year']
     ordering = ['title'] 
+
+    
